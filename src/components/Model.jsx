8b788626687981e74fc716996468ddb1f8be5747
Argjent <argjent.larv@gmail.com>
2025-03-10 22:08:30 +0100

    Workign on Model

---
+++ src/components/Model.jsx
@@ -0,0 +1,26 @@
+import { useGSAP } from "@gsap/react"
+import React from 'react'
+
+const Model = () => {
+    useGSAP(() => {
+        gsap.to('#heading', { y: 0, opacity: 1})
+    }, [])
+
+
+  return (
+    <section className="common-padding">
+        <div className="screen-max-width">
+            <h1 id="heading" className="section-heading">
+            Regardez de plus près.
+            </h1>
+            <div className="flex flex-col items-center mt-5">
+                <div className="w-full h-[75vh] md:h-[90vh] overflow-hidden relative">
+
+                </div>
+            </div>
+        </div>
+    </section>
+  )
+}
+
+export default Model

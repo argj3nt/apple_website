534ba8648db556b20f684817a8f27b576141e445
Argjent <argjent.larv@gmail.com>
2025-04-04 19:17:33 +0200

    Working on Model

--- src/components/Model.jsx
+++ src/components/Model.jsx
@@ -15,7 +15,7 @@ const Model = () => {
             Regardez de plus près
             </h1>
             <div className="flex flex-col items-center mt-5">
-                <div className="w-full h-[75vh] md:h-[90vh] overflow-hidden relativeeeeee">
+                <div className="w-full h-[75vh] md:h-[90vh] overflow-hidden relative">
                 <ModelView />
                 </div>
             </div>

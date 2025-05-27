d4aa165d70f5d219722c23159acd8d3f9e651d23
Argjent <argjent.larv@gmail.com>
2025-05-27 20:28:03 +0200

    Finished the Model

--- src/components/Model.jsx
+++ src/components/Model.jsx
@@ -10,7 +10,7 @@ import { yellowImg } from "../utils";
 import * as THREE from 'three';
 import { Canvas } from "@react-three/fiber";
 import { View } from "@react-three/drei";
-import { models } from "../constants";
+import { models, sizes } from "../constants";
 
 const Model = () => {
     const [size, setsize] = useState('small');
@@ -87,11 +87,18 @@ const Model = () => {
                     <ul className="color-container">
                         {models.map((item, i) => (
                             <li key={i} className="w-6 h-6 rounded-full mx-2" style={{backgroundColor: item.color[0] }} 
-                            onClick={() => setModel}>
-
-                            </li>
+                            onClick={() => setModel(item)} />
                         ))}
                     </ul>
+
+                    <button className="size-btn-container">
+                {sizes.map(({ label, value }) => (
+                  <span key={label} className="size-btn" style={{ backgroundColor: size === value ? 'white' : 'transparent', color: size === value ? 'black' : 'white'}} onClick={() => setSize(value)}>
+                    {label}
+                  </span>
+                ))}
+              </button>
+
                 </div>
                 </div>
             </div>
